ec596bffa6632b2ec5bee56c1aec6948261688f7
Triet-Cen <caenavon4869@gmail.com>
2026-04-02 01:25:23 +0700

    Keep empty folders

---
+++ warehouseqr/src/main/java/com/tttn/warehouseqr/WarehouseqrApplication.java
@@ -0,0 +1,13 @@
+package com.tttn.warehouseqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseqrApplication.class, args);
+	}
+
+}
