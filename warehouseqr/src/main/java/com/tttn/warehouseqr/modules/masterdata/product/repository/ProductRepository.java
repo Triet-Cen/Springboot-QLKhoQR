b4b1f45f737ce601c1f207d10c1a11f2975d9be8
DoanHoangHuy <oopshuypvp@gmail.com>
2026-04-06 11:30:10 +0700

    Product crud

---
+++ warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/product/repository/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.tttn.warehouseqr.modules.masterdata.product.repository;
+
+import com.tttn.warehouseqr.modules.masterdata.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long>{
+    @Query(value = "SELECT p FROM Product p " +
+            "WHERE (:keyw IS NULL OR p.productName LIKE CONCAT('%', :keyw, '%')) " +
+            "AND (:categoryId = 0 OR p.category.categoryId = :categoryId) ")
+    Page<Product> getProducPageCustom(@Param("keyw") String keyw, @Param("categoryId") long categoryId, Pageable page);
+
+//    @Query(value = "SELECT COUNT(p.product_id) " +
+//            "FROM Product p " +
+//            "WHERE (:keyw IS NULL OR p.productName LIKE CONCAT('%', :keyw, '%')) " +
+//            "AND (:categoryId = 0 OR p.category.categoryId = :categoryId)",nativeQuery = true)
+//    long CountTotalElements(@Param("keyw") String keyw, @Param("categoryId") long categoryId);
+}
