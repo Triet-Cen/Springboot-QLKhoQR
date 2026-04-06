03c515462d06a9f1453dca05399c8b9ad2021ac1
Doan Hoang Huy <oopshuypvp@gmail.com>
2026-04-06 21:47:24 +0700

    Revert "Product qr code 90%"

--- warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tttn.warehouseqr.modules.masterdata.category.repository;
-
-import com.tttn.warehouseqr.modules.masterdata.category.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<ProductCategory,Long> {
-}
--- warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/unit/repository/UnitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tttn.warehouseqr.modules.masterdata.unit.repository;
-
-import com.tttn.warehouseqr.modules.masterdata.unit.entity.Unit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UnitRepository extends JpaRepository<Unit,Long> {
-}
