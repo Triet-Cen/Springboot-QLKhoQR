c8f498128627663509966dbb6b620f4e3f5acc6d
Triet-Cen <caenavon4869@gmail.com>
2026-04-11 17:57:16 +0700

    Basic cus,comp and sup

--- warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/category/repository/CategoryRepository.java
+++ warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/category/repository/CategoryRepository.java
@@ -1,17 +1,10 @@
 package com.tttn.warehouseqr.modules.masterdata.category.repository;
 
 import com.tttn.warehouseqr.modules.masterdata.category.entity.ProductCategory;
-import com.tttn.warehouseqr.modules.masterdata.supplier.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<ProductCategory,Long> {
-    boolean existsBySupplierCode(String supplierCode);
-    boolean existsBySupplierId(Long id);
-    List<Supplier> search(@Param("keyword") String keyword);
 
 }
