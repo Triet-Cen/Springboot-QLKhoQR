0aefe573961b4c72f25aef4573caa70c71c595dc
DoanHoangHuy <oopshuypvp@gmail.com>
2026-04-14 22:31:18 +0700

    Thêm bảng tạm

--- warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/product/service/impl/ImportQrService.java
+++ warehouseqr/src/main/java/com/tttn/warehouseqr/modules/masterdata/product/service/impl/ImportQrService.java
@@ -29,6 +29,8 @@ import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -57,130 +59,165 @@ public class ImportQrService {
     }
 
     @Transactional
-    public void importCsvAndGenerateQr(MultipartFile file){
-        try (BufferedReader fileRender = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
-             CSVParser csvParser = new CSVParser(fileRender, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())){
+    public void importCsvAndGenerateQr(MultipartFile file) {
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(file.getInputStream(),
+                StandardCharsets.UTF_8))) {
 
-            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+            String headerLine = fileReader.readLine();
+            if (headerLine == null) {
+                throw new RuntimeException("File CSV trống!");
+            }
 
-            for(CSVRecord csvRecord : csvRecords){
-                String sku = csvRecord.get("SKU");
-                String productName = csvRecord.get("Tên Sản phẩm");
-                String description = csvRecord.get("Mô Tả");
-                String minStock = csvRecord.get("Tồn Kho Tối Thiểu");
-                String categoryId = csvRecord.get("Mã Danh Mục");
-                String unitId = csvRecord.get("Mã Đơn Vị");
+            if (headerLine.startsWith("\uFEFF")) {
+                headerLine = headerLine.substring(1);
+            }
 
-                String batchCode = csvRecord.get("Mã Lô Hàng");
-                String serialNum = csvRecord.get("Serial Number");
-                String costPrice = csvRecord.get("Giá Nhập");
-                String expiryDate = csvRecord.get("Ngày Hết Hạn");
-                String  supplierId = csvRecord.get("Mã NCC");
-                Long parseSupplierId = parseLong(supplierId, null);
+            String[] cleanHeaders = Arrays.stream(headerLine.split(","))
+                    .map(String::trim)
+                    .toArray(String[]::new);
 
-                String qtyStr = csvRecord.isSet("Số Lượng") ? csvRecord.get("Số Lượng") : "0";
-                String locationCodeStr = csvRecord.isSet("Mã Vị Trí") ? csvRecord.get("Mã Vị Trí") : "";
+            try (CSVParser csvParser = new CSVParser(fileReader,
+                    CSVFormat.DEFAULT.withHeader(cleanHeaders).withIgnoreHeaderCase().withTrim())) {
 
+                Iterable<CSVRecord> csvRecords = csvParser.getRecords();
 
-                Product product = productRepository.findBySku(sku);
-                if(product == null){
-                    product = new Product();
-                    product.setSku(sku);
-                    product.setProductName(productName);
-                    product.setDescription(description.isEmpty() ? null : description);
-                    product.setMinStock(parseBigDecimal(minStock));
+                for (CSVRecord csvRecord : csvRecords) {
+                    String sku = csvRecord.get("SKU");
+                    String productName = csvRecord.get("Tên Sản Phẩm"); // Chữ 'P' viết hoa theo chuẩn file CSV
+                    String description = csvRecord.get("Mô Tả");
+                    String minStock = csvRecord.get("Tồn Kho Tối Thiểu");
+                    String categoryId = csvRecord.get("Mã Danh Mục");
+                    String unitId = csvRecord.get("Mã Đơn Vị");
 
-                    ProductCategory category = new ProductCategory();
-                    category.setCategoryId(parseLong(categoryId, 1L));
-                    product.setCategory(category);
+                    String batchCode = csvRecord.get("Mã Lô Hàng");
+                    String serialNum = csvRecord.get("Serial Number");
+                    String costPrice = csvRecord.get("Giá Nhập");
+                    String expiryDate = csvRecord.get("Ngày Hết Hạn");
+                    String supplierId = csvRecord.get("Mã NCC");
+                    Long parseSupplierId = parseLong(supplierId, null);
 
-                    Unit unit = new Unit();
-                    unit.setUnitId(parseLong(unitId, 1L));
-                    product.setUnit(unit);
+                    String qtyStr = csvRecord.isSet("Số Lượng") ? csvRecord.get("Số Lượng") : "0";
+                    String locationCodeStr = csvRecord.isSet("Mã Vị Trí") ? csvRecord.get("Mã Vị Trí") : "";
+                    String warehouseIdStr = csvRecord.isSet("Mã Kho") ? csvRecord.get("Mã Kho") : "1";
 
-                    productRepository.save(product);
-                }
+                    Long warehouseId = parseLong(warehouseIdStr, 1L);
 
-                ProductBatch batch = new ProductBatch();
-                batch.setLotCode(batchCode);
-                batch.setSerialNumber(serialNum.isEmpty() ? null : serialNum);
-                batch.setCostPrice(parseBigDecimal(costPrice));
-                if(parseSupplierId != null){
-                    Supplier supplier = new Supplier();
-                    supplier.setSupplierId(parseSupplierId);
-                    batch.setSupplier(supplier);
-                }
-                batch.setProduct(product);
+                    Product product = productRepository.findBySku(sku);
+                    if (product == null) {
+                        product = new Product();
+                        product.setSku(sku);
+                        product.setProductName(productName);
+                        product.setDescription(description.isEmpty() ? null : description);
+                        product.setMinStock(parseBigDecimal(minStock));
 
-                if (!expiryDate.isEmpty()){
-                    batch.setExpiryDate(LocalDate.parse(expiryDate));
-                }
-                batch = productBatchRepository.save(batch);
+                        ProductCategory category = new ProductCategory();
+                        category.setCategoryId(parseLong(categoryId, 1L));
+                        product.setCategory(category);
 
-                String qrContent = sku + "|" + batchCode;
-                String base64Image = QrCodeUtil.GenerateQRCodeBase64(qrContent,300,300);
+                        Unit unit = new Unit();
+                        unit.setUnitId(parseLong(unitId, 1L));
+                        product.setUnit(unit);
 
-                QrCode qrCode = new QrCode();
-                qrCode.setQrContent(qrContent);
-                qrCode.setImgPath(base64Image);
-                qrCode.setReferenceType("BATCH");
-                qrCode.setReferenceId(batch.getBatchId());
-                qrCode.setPrinted(false);
-
-                qrCodeResipotory.save(qrCode);
-
-                double importQty = 0;
-                try {
-                    importQty = Double.parseDouble(qtyStr);
-                }
-                catch (NumberFormatException e){
-                    importQty = 0;
-                }
-
-                if(importQty > 0 && !locationCodeStr.isEmpty()){
-                    WarehouseLocation location = warehouseLocationRepository.findByLocationCode(locationCodeStr).orElseGet(
-                            () -> {
-                                WarehouseLocation newLocation = new WarehouseLocation();
-                                newLocation.setLocationCode(locationCodeStr);
-                                com.tttn.warehouseqr.modules.masterdata.warehouse.entity.Warehouse defaultWarehouse = new com.tttn.warehouseqr.modules.masterdata.warehouse.entity.Warehouse();
-                                defaultWarehouse.setWarehouseId(1L);
-                                newLocation.setWarehouses(defaultWarehouse);
-                                return warehouseLocationRepository.save(newLocation);
-                            }
-                    );
-
-                    Optional<InventoryLocationBalance> balanceOpt = inventoryLocationBalanceRepository.findByBatchIdAndLocationId(batch.getBatchId(), location.getLocationId());
-                    InventoryLocationBalance balance;
-
-                    if (balanceOpt.isPresent()){
-                        balance = balanceOpt.get();
-                        BigDecimal currentQty = balance.getQty() != null ? balance.getQty() : BigDecimal.ZERO;
-                        balance.setQty(currentQty.add(BigDecimal.valueOf(importQty)));
+                        productRepository.save(product);
                     }
-                    else {
-                        balance = new InventoryLocationBalance();
-                        balance.setBatchId(batch.getBatchId());
-                        balance.setLocationId(location.getLocationId());
-                        balance.setProductId(product.getProduct_id());
-                        balance.setWarehouseId(location.getWarehouses().getWarehouseId());
-                        balance.setQty(BigDecimal.valueOf(importQty));
-                    }
-                    balance.setUpdateAt(LocalDateTime.now());
-                    inventoryLocationBalanceRepository.save(balance);
 
-                    InventoryHistory history = new InventoryHistory();
-                    history.setTransactionType("INITIAL_IMPORT");
-                    history.setProductId(product.getProduct_id());
-                    history.setBatchId(batch.getBatchId());
-                    history.setToLocationId(location.getLocationId());
-                    history.setQtyChange(BigDecimal.valueOf(importQty));
-                    history.setWarehouseId(location.getWarehouses().getWarehouseId());
-                    history.setCreatedAt(LocalDateTime.now());
-                    inventoryHistoryRepository.save(history);
+                    ProductBatch batch = productBatchRepository.
+                            findByLotCodeAndProductProduct_id(batchCode, product.getProduct_id())
+                            .orElse(null);
+                    boolean isNewBatch = false;
+
+                    if (batch == null) {
+                        batch = new ProductBatch();
+                        batch.setLotCode(batchCode);
+                        batch.setSerialNumber(serialNum.isEmpty() ? null : serialNum);
+                        batch.setCostPrice(parseBigDecimal(costPrice));
+                        if (parseSupplierId != null) {
+                            Supplier supplier = new Supplier();
+                            supplier.setSupplierId(parseSupplierId);
+                            batch.setSupplier(supplier);
+                        }
+                        batch.setProduct(product);
+
+                        if (!expiryDate.isEmpty()) {
+                            batch.setExpiryDate(parseFlexibleDate(expiryDate));
+                        }
+                        batch = productBatchRepository.save(batch);
+                        isNewBatch = true;
+                    } else {
+                        throw new RuntimeException("Lô hàng '" + batchCode + "' của sản phẩm '"
+                                + sku + "' đã tồn tại! Hệ thống đã hủy quá trình Import.");
+                    }
+
+                    // XỬ LÝ QR CODE
+                    if (isNewBatch) {
+                        String qrContent = sku + "|" + batchCode;
+                        String base64Image = QrCodeUtil.GenerateQRCodeBase64(qrContent, 300, 300);
+
+                        QrCode qrCode = new QrCode();
+                        qrCode.setQrContent(qrContent);
+                        qrCode.setImgPath(base64Image);
+                        qrCode.setReferenceType("BATCH");
+                        qrCode.setReferenceId(batch.getBatchId());
+                        qrCode.setPrinted(false);
+
+                        qrCodeResipotory.save(qrCode);
+                    }
+
+                    // XỬ LÝ TỒN KHO & VỊ TRÍ KỆ
+                    double importQty = 0;
+                    try {
+                        importQty = Double.parseDouble(qtyStr);
+                    } catch (NumberFormatException e) {
+                        importQty = 0;
+                    }
+
+                    if (importQty > 0 && !locationCodeStr.isEmpty()) {
+                        WarehouseLocation location = warehouseLocationRepository
+                                .findByLocationCodeAndWarehouses_WarehouseId(locationCodeStr,warehouseId).orElseGet(
+                                () -> {
+                                    WarehouseLocation newLocation = new WarehouseLocation();
+                                    newLocation.setLocationCode(locationCodeStr);
+                                    com.tttn.warehouseqr.modules.masterdata.warehouse.entity.Warehouse defaultWarehouse
+                                            = new com.tttn.warehouseqr.modules.masterdata.warehouse.entity.Warehouse();
+                                    defaultWarehouse.setWarehouseId(warehouseId);
+                                    newLocation.setWarehouses(defaultWarehouse);
+
+                                    return warehouseLocationRepository.save(newLocation);
+                                }
+                        );
+
+                        Optional<InventoryLocationBalance> balanceOpt = inventoryLocationBalanceRepository.
+                                findByBatchIdAndLocationId(batch.getBatchId(), location.getLocationId());
+                        InventoryLocationBalance balance;
+
+                        if (balanceOpt.isPresent()) {
+                            balance = balanceOpt.get();
+                            BigDecimal currentQty = balance.getQty() != null ? balance.getQty() : BigDecimal.ZERO;
+                            balance.setQty(currentQty.add(BigDecimal.valueOf(importQty)));
+                        } else {
+                            balance = new InventoryLocationBalance();
+                            balance.setBatchId(batch.getBatchId());
+                            balance.setLocationId(location.getLocationId());
+                            balance.setProductId(product.getProduct_id());
+                            balance.setWarehouseId(location.getWarehouses().getWarehouseId());
+                            balance.setQty(BigDecimal.valueOf(importQty));
+                        }
+                        balance.setUpdateAt(LocalDateTime.now());
+                        inventoryLocationBalanceRepository.save(balance);
+
+                        InventoryHistory history = new InventoryHistory();
+                        history.setTransactionType("INITIAL_IMPORT");
+                        history.setProductId(product.getProduct_id());
+                        history.setBatchId(batch.getBatchId());
+                        history.setToLocationId(location.getLocationId());
+                        history.setQtyChange(BigDecimal.valueOf(importQty));
+                        history.setWarehouseId(location.getWarehouses().getWarehouseId());
+                        history.setCreatedAt(LocalDateTime.now());
+                        inventoryHistoryRepository.save(history);
+                    }
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Lỗi khi đọc file CSV: " + e.getMessage());
         }
     }
@@ -195,6 +232,30 @@ public class ImportQrService {
         try { return Long.parseLong(value.trim()); } catch (Exception e) { return defaultValue; }
     }
 
+    private LocalDate parseFlexibleDate(String dateStr) {
+        if (dateStr == null || dateStr.trim().isEmpty()) return null;
+        String cleanDate = dateStr.trim();
+
+        List<DateTimeFormatter> formatters = Arrays.asList(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd"),
+                DateTimeFormatter.ofPattern("M-d-yyyy"),
+                DateTimeFormatter.ofPattern("MM-dd-yyyy"),
+                DateTimeFormatter.ofPattern("d-M-yyyy"),
+                DateTimeFormatter.ofPattern("dd-MM-yyyy"),
+                DateTimeFormatter.ofPattern("yyyy/MM/dd"),
+                DateTimeFormatter.ofPattern("M/d/yyyy"),
+                DateTimeFormatter.ofPattern("d/M/yyyy")
+        );
+
+        for (DateTimeFormatter formatter : formatters) {
+            try {
+                return LocalDate.parse(cleanDate, formatter);
+            } catch (Exception e) {
+            }
+        }
+        throw new RuntimeException("Hệ thống không hiểu định dạng ngày: " + cleanDate + ". Vui lòng dùng định dạng Năm-Tháng-Ngày hoặc Ngày-Tháng-Năm.");
+    }
+
     @Transactional
     public void generateManualQr(List<Long> batchIds) {
         for (Long batchId : batchIds) {
@@ -203,6 +264,20 @@ public class ImportQrService {
             if (existingQr == null) {
                 ProductBatch batch = productBatchRepository.findById(batchId).orElse(null);
                 if (batch != null) {
+                    List<InventoryLocationBalance> balances = inventoryLocationBalanceRepository.findByBatchId(batchId);
+                    BigDecimal totalQty = BigDecimal.ZERO;
+                    String locationCode = "Chưa-Xếp-Kệ";
+                    String warehouseInfo = "Chưa-Rõ-Kho";
+
+                    if (balances != null && !balances.isEmpty()){
+                        // Lấy kệ đầu tiên làm đại diện
+                        InventoryLocationBalance findBalence = balances.get(0);
+                        //Cộng dồn số lượng
+                        for (InventoryLocationBalance b : balances){
+                            totalQty = totalQty.add(b.getQty() != null ? b.getQty() : BigDecimal.ZERO);
+                        }
+
+                    }
 
                     String qrContent = batch.getProduct().getSku() + "|" + batch.getLotCode();
 
