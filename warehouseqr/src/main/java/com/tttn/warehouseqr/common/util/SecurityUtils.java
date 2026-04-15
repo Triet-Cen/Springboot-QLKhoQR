f8524bbf2edcd721a371732de242769b1f283dce
HoDucDat <ducdat519@gmail.com>
2026-04-15 16:40:24 +0700

    hoan thanh

---
+++ warehouseqr/src/main/java/com/tttn/warehouseqr/common/util/SecurityUtils.java
@@ -0,0 +1,15 @@
+package com.tttn.warehouseqr.common.util;
+
+import com.tttn.warehouseqr.modules.auth.entity.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+    public static Long getCurrentUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof CustomUserDetails userDetails) {
+            return userDetails.getUserId();
+        }
+        return null; // Hoặc ném ra một Exception nếu bắt buộc phải đăng nhập
+    }
+}
